4ac0c334caa96643f6537ab05db100d1403525b7
yubarajsingh <yubaraj605@gmail.com>
2020-12-13 08:33:52 +0545

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('layouts.app')
 
 @section('css')
@@ -26,67 +24,143 @@
 
 
   <div class="row">
-    @foreach($trips as $trip)
-    @php
-      $count=0; 
-      @endphp
-      @foreach ($images as $image)
-      
-      @if ($trip->id==$image->trip_id and $count==0)
-     
-      
-      <div class="col-lg-4 col-md-6 col-sm-6 my-3">
-        <div class="card">
-          <img src="{{asset('storage/'.$image->image)}}" alt="pokhara" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title">{{$trip->title}}</h5>
-            {{-- <p class="card-text">description if needed</p> --}}
-            
-            <a href="{{route('tripdetail.show',$trip->id)}}" class="btn btn-primary br-5">Book now</a>
-     
-          </div>
-        </div>
-      </div>
+    <div class="owl-carousel owl-theme">
+      @foreach($trips as $trip)
       @php
-      $count=1
-     @endphp
-     @endif
-      @endforeach
-      @endforeach 
-<div class="heading"> 
+        $count=0; 
+        @endphp
+        @foreach ($images as $image)
+        
+        @if ($trip->id==$image->trip_id and $count==0)
+       
+        
+        
+          <div class="card">
+            <img src="{{asset('storage/'.$image->image)}}" alt="pokhara" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">{{$trip->title}}</h5>
+              {{-- <p class="card-text">description if needed</p> --}}
+              
+              <a href="{{route('tripdetail.show',$trip->id)}}" class="btn btn-primary br-5">Book now</a>
+       
+            </div>
+      
+        </div>
+        @php
+        $count=1
+       @endphp
+       @endif
+        @endforeach
+        @endforeach 
+      </div>
+  </div>
+  <div class="heading">
+    <h1>Recomended</h1>
+  </div>
+  
+  
+    <div class="row">
+      <div class="owl-carousel owl-theme">
+        @foreach($trips as $trip)
+        @php
+          $count=0; 
+          @endphp
+          @foreach ($images as $image)
+          
+          @if ($trip->id==$image->trip_id and $count==0)
+         
+          
+          
+            <div class="card">
+              <img src="{{asset('storage/'.$image->image)}}" alt="pokhara" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">{{$trip->title}}</h5>
+                {{-- <p class="card-text">description if needed</p> --}}
+                
+                <a href="{{route('tripdetail.show',$trip->id)}}" class="btn btn-primary br-5">Book now</a>
+         
+              </div>
+        
+          </div>
+          @php
+          $count=1
+         @endphp
+         @endif
+          @endforeach
+          @endforeach 
+        </div>
+    </div>
+      <div class="heading"> 
   <h2>Most visited Places</h2>
 
 </div>
 
-@foreach($trips as $trip)
-@php
-  $count=0; 
-  @endphp
-  @foreach ($images as $image)
-  
-  @if ($trip->id==$image->trip_id and $count==0)
- 
-  
-  <div class="col-lg-4 col-md-6 col-sm-6 my-3">
-    <div class="card">
-      <img src="{{asset('storage/'.$image->image)}}" alt="pokhara" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">{{$trip->title}}</h5>
-        {{-- <p class="card-text">description if needed</p> --}}
-        
-        <a href="{{route('tripdetail.show',$trip->id)}}" class="btn btn-primary br-5">Book now</a>
- 
-      </div>
-    </div>
-  </div>
+<div class="owl-carousel owl-theme">
+  @foreach($trips as $trip)
   @php
-  $count=1
- @endphp
- @endif
-  @endforeach
-  @endforeach
+    $count=0; 
+    @endphp
+    @foreach ($images as $image)
+    
+    @if ($trip->id==$image->trip_id and $count==0)
+   
+    
+    
+      <div class="card">
+        <img src="{{asset('storage/'.$image->image)}}" alt="pokhara" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">{{$trip->title}}</h5>
+          {{-- <p class="card-text">description if needed</p> --}}
+          
+          <a href="{{route('tripdetail.show',$trip->id)}}" class="btn btn-primary br-5">Book now</a>
+   
+        </div>
+  
+    </div>
+    @php
+    $count=1
+   @endphp
+   @endif
+    @endforeach
+    @endforeach 
+  </div>
 
-
+  <div class="heading">
+    <h1>Cheap Destination</h1>
+  </div>
+  
+  
+    <div class="row">
+      <div class="owl-carousel owl-theme">
+        @foreach($trips as $trip)
+        @php
+          $count=0; 
+          @endphp
+          @foreach ($images as $image)
+          
+          @if ($trip->id==$image->trip_id and $count==0)
+         
+          
+          
+            <div class="card">
+              <img src="{{asset('storage/'.$image->image)}}" alt="pokhara" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">{{$trip->title}}</h5>
+                {{-- <p class="card-text">description if needed</p> --}}
+                
+                <a href="{{route('tripdetail.show',$trip->id)}}" class="btn btn-primary br-5">Book now</a>
+         
+              </div>
+        
+          </div>
+          @php
+          $count=1
+         @endphp
+         @endif
+          @endforeach
+          @endforeach 
+        </div>
+    </div>
 {{-- <div class="row">
   @foreach ($trips as $trip)
   <div class="card">
@@ -125,4 +199,45 @@
 
 @section('footer')
     @include('layouts.footer')
+@endsection
+@section('css')
+<link rel="stylesheet" href="{{ URL::asset('css/owl.css') }}" />
+@endsection
+@section('scripts')
+<style>
+  .owl-carousel .item {
+    height: 100rem;
+    background: #4DC7A0;
+    padding: 1rem;
+  }
+  .owl-carousel .item h4 {
+    color: #FFF;
+    font-weight: 400;
+    margin-top: 0rem;
+   }
+  </style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.green.min.css"/>
+  <script>
+  jQuery(document).ready(function($){
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:2
+        },
+        1000:{
+          items:4
+        }
+      }
+    })
+  })
+  </script>
 @endsection
